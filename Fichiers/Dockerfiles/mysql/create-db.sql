4820ce73c8675e9b0bd1ddf38799dbfa5a7bcc34
Nico Mengisen <mengisennico@gmail.com>
2023-05-18 23:42:17 +0200

    debuging the mysql database for wp

--- Fichiers/Dockerfiles/mysql/create-db.sql
+++ Fichiers/Dockerfiles/mysql/create-db.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH mysql_native_password BY 'wordpress';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+CREATE USER 'wordpress'@'%' IDENTIFIED WITH mysql_native_password BY 'wordpress';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-
