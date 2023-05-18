7c4de2cd3f9291b3480f03e0308bd149acda1507
Nico Mengisen <mengisennico@gmail.com>
2023-05-18 22:39:08 +0200

    QUIT; removed

--- Fichiers/Dockerfiles/mysql/create-db.sql
+++ Fichiers/Dockerfiles/mysql/create-db.sql
@@ -2,4 +2,4 @@ CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH mysql_native_password BY 'wordpress';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
 FLUSH PRIVILEGES;
-QUIT;
+
