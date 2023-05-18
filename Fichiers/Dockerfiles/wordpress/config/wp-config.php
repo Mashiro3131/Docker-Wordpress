3f87cdc45d324485bd9ed8aef0a5bd86cc5d0f6a
Nico Mengisen <mengisennico@gmail.com>
2023-05-18 22:43:15 +0200

    Synthax errors fixed

--- Fichiers/Dockerfiles/wordpress/config/wp-config.php
+++ Fichiers/Dockerfiles/wordpress/config/wp-config.php
@@ -1,31 +1,18 @@
 <?php
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
-
-/** MySQL database username */
 define('DB_USER', 'wordpress');
-
-/** MySQL database password */
-define('DB_PASSWORDdefine', 'wordpress');
-
- /** MySQL hostname */
+define('DB_PASSWORD', 'wordpress');
 define('DB_HOST', 'mengisen-projetdocker-mysql-server');
-
-/** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
-/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-/** WordPress debug mode for developers */
 define('WP_DEBUG', false);
 
-// Define additional paths for Apache
 define('WP_CONTENT_DIR', '/var/www/html/wp-content');
 define('WP_CONTENT_URL', 'http://localhost/wp-content');
+// define('WP_SITEURL', 'http://localhost');
+// define('WP_HOME', 'http://localhost');
+// define('WP_DEFAULT_THEME', 'ctlg-wpcom');
 
-// Path to the WP theme
+
 define('TEMPLATEPATH', '/var/www/html/wp-content/themes/ctlg-wpcom/templates');
 define('STYLESHEETPATH', '/var/www/html/wp-content/themes/ctlg-wpcom/styles');
-
