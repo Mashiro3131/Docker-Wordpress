1d20e5d569c32edca368b48508c008428b6f85d8
Nico Mengisen <mengisennico@gmail.com>
2023-05-19 16:00:02 +0200

    Update

--- Fichiers/Dockerfiles/wordpress/config/wp-config.php
+++ Fichiers/Dockerfiles/wordpress/config/wp-config.php
@@ -1,18 +1,28 @@
 <?php
+
 define('DB_NAME', 'wordpress');
-define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'wordpress');
+
+define('DB_USER', 'nico');
+define('DB_PASSWORD', 'wp');
+
+define('DB_Port', '3308');
+
+define('DB_ROOT_USER', 'root');
+define("DB_ROOT_PASSWORD", "wp");
+
 define('DB_HOST', 'mengisen-projetdocker-mysql-server');
+
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 define('WP_DEBUG', true);
 
 define('WP_CONTENT_DIR', '/var/www/html/wp-content');
-define('WP_CONTENT_URL', 'http://127.0.0.1/wp-content');
-define('WP_SITEURL', 'http://127.0.0.1');
-define('WP_HOME', 'http://127.0.0.1');
-define('WP_DEFAULT_THEME', 'ctlg-wpcom');
+define('WP_CONTENT_URL', 'http://localhost/wp-content');
 
+define('WP_SITEURL', 'http://localhost');
+define('WP_HOME', 'http://localhost');
+
+define('WP_DEFAULT_THEME', 'ctlg-wpcom');
 
 define('TEMPLATEPATH', '/var/www/html/wp-content/themes/ctlg-wpcom/templates');
 define('STYLESHEETPATH', '/var/www/html/wp-content/themes/ctlg-wpcom/styles');
