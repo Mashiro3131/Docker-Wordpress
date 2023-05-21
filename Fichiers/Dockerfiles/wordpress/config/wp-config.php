6eaee80e8a55cd7129f49df803ce5fbb1d60bc1b
Nico Mengisen <mengisennico@gmail.com>
2023-05-21 15:11:08 +0200

    No more ERROR establishing connection to DB

--- Fichiers/Dockerfiles/wordpress/config/wp-config.php
+++ Fichiers/Dockerfiles/wordpress/config/wp-config.php
@@ -1,39 +1,104 @@
 <?php
 
-# Database configuration
 
+// define( 'DB_NAME', 'wordpress' );
+// define( 'DB_USER', 'wordpress' );
+// define( 'DB_PASSWORD', 'wordpress' );
+// define( 'DB_HOST', 'mysql:3306' );
+
+
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
 define( 'DB_NAME', 'wordpress' );
+
+/** Database username */
 define( 'DB_USER', 'wordpress' );
+
+/** Database password */
 define( 'DB_PASSWORD', 'wordpress' );
-define( 'DB_HOST', 'mysql:3306' );
 
+/** Database hostname */
+define( 'DB_HOST', 'mysql' );
 
-// # Security keys
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
 
-// define( 'AUTH_KEY',         'put your unique phrase here' );
-// define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-// define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-// define( 'NONCE_KEY',        'put your unique phrase here' );
-// define( 'AUTH_SALT',        'put your unique phrase here' );
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
-// # Table prefix
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'Adv=( U)i!i_4oN<q>rPQX[P$|=mzv |~.xZ} ;anh;x[J8KgBx2Ac7dZfJILQXL');
+define('SECURE_AUTH_KEY',  'Sg`yWc~4nteX${Iq/b3e20,94@2E:4oX-#T3ZQ`rjw?pf%*FaKUG<WC+^stZK(+T');
+define('LOGGED_IN_KEY',    'XLPaMCz[QAZ=F~3+^*m0-ZP[urmCbO`!+>9@gsln9-LXSCE+-:[(i=E|$2j`dBH~');
+define('NONCE_KEY',        'BKidR!2%ng<IDi2,CwDZz%5M|kAxhl#-$.A=!JWJSod+jMs.<,fv>+F;r=W:Ghi-');
+define('AUTH_SALT',        'hQ=^cCCjk*.6iQsznDx~@*=-8bR,VwGPh,hugHHFV^lRS|=wff1OJP?4%OGR{PTM');
+define('SECURE_AUTH_SALT', 'eP.wLX3s#FNeU6TFvFg/>gi4QPXJ)pJE!R;+~0,i[lm3NmPP`rn6<l@IibQsQHDe');
+define('LOGGED_IN_SALT',   ',lA UtZuKzLJS?o8(Gk+@>:H9|h(T3ZM4/}b|?6J}{Zr%&v3A|-*.-&5],%$)+B!');
+define('NONCE_SALT',       '|[^(Zz8y$v!E%9?NzQ!y|15F/sh3e]L8aH lccf[li.La%3(@h_t_HGloihEV+aU');
 
-// $table_prefix = 'wp_';
+/**#@-*/
 
-// # Debug mode
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
 
-// define( 'WP_DEBUG', false );
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
 
-// # Absolute path to the WordPress directory
+/* Add any custom values between this line and the "stop editing" line. */
 
-// if ( ! defined( 'ABSPATH' ) ) {
-//     define( 'ABSPATH', __DIR__ . '/' );
-// }
+define('WP_ALLOW_REPAIR', true);
 
-// # Sets up WordPress vars and included files
+/* That's all, stop editing! Happy publishing. */
 
-// require_once ABSPATH . 'wp-settings.php';
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
 
-// // Path: Fichiers\Dockerfiles\wordpress\docker-compose.yml
-
-
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
--- Fichiers/scripts/start.sh
+++ Fichiers/scripts/start.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# chmod +x "$0"
+chmod +x "$0"
 
-# # docker-compose up -d mengisen-projetdocker-mysql-server
 
-# docker-compose up -d --build
-# echo
-# docker-compose ps
-# echo
-# docker images --all
-
+# DOCKER-COMPOSE
 
 # Supprime les anciens conteneurs
 docker-compose down --volumes --remove-orphans
@@ -20,16 +14,37 @@ docker system prune -a --volumes --force
 # Construit les images et lance les conteneurs
 docker-compose up -d --build
 
+# MYSQL
+
 # Wait for the MySQL container to be ready
 until docker-compose exec mysql mysqladmin ping -h "mysql" --silent; do
     echo "La patience est une vertue, maintenant attendez que le conteneur mysql soit prêt..."
     sleep 2
 done
 
-# Create the WordPress database and user
+# Crée les utilisateurs et la base de données
 docker-compose exec mysql mysql -u root -prootpress -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 docker-compose exec mysql mysql -u root -prootpress -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
 docker-compose exec mysql mysql -u root -prootpress -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
 docker-compose exec mysql mysql -u root -prootpress -e "FLUSH PRIVILEGES;"
 
 echo "WordPress tourne sur http://127.0.0.1:8080"
+
+
+
+# APACHE
+
+# # Check if ServerName is already set in apache2.conf
+# if grep -q "ServerName" /etc/apache2/apache2.conf; then
+#     echo "ServerName est déjà configuré dans apache2.conf"
+# else
+#     echo "Avec l'age vient la sagesse, maintenant faut attendre que le conteneur wordpress soit prêt..."
+#     while ! docker-compose logs wordpress | grep -q "WordPress installation complete"; do
+#         sleep 1
+#     done
+
+#     echo "Adding ServerName to apache2.conf"
+#     add_servername
+# fi
+
+# service apache2 restart
